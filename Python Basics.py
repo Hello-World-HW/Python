5be41c49c591c21bbd5e61282c9bdb546cfc1d47
Hello World <hello.world.hw.official@gmail.com>
2025-01-24 15:53:55 +0100

    Add files via upload

---
+++ Python Basics.py
@@ -0,0 +1,16 @@
+########## What Are Variables? ##########
+name = 'Alice'
+age = 25
+height = 5.6
+########## Data Types in Python ##########
+greeting = 'Hello, World!'
+number = 42
+price = 19.99
+is_active = True
+result = None
+print(type(name))
+########## Dynamic Typing in Python ##########
+x = 10
+x = 'Python'
+########## Using Variables in Practice ##########
+print(f'{name} is {age} years old and is {height} feet tall.')
